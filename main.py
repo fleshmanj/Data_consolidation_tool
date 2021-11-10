57724a6cb737b3de1baad142107bcfd1af0cea35
Joshua <fleshmanjoshuac@hotmail.com>
2021-11-09 19:32:46 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+import glob
+import docx2txt
+import datetime
+from docx import Document
+
+
+DATETIME = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Extracts bullets from collection of files.')
+    parser.add_argument("-i", "--input", type=str, help='Enter folder address. Use /filepath\**\*.docx format')
+    args = parser.parse_args()
+
+
+def extract_bullets(raw):
+    batch = glob.glob(raw, recursive=True)
+    document = Document()
+    for file in batch:
+        print(str(file))
+        data = docx2txt.process(file)
+        data = "".join(data)
+        data = data.split("\n\n")
+        for bullet in data:
+            if bullet == "":
+                continue
+            bullet = bullet.strip()
+            if bullet.startswith("(") or bullet.startswith("-"):
+                document.add_paragraph(bullet)
+    document.save(f"Bullet extract {DATETIME}.docx")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extracts bullets from collection of files.')
+    parser.add_argument("-i", "--input", type=str, help='Enter folder location. Use /filepath\**\*.docx format')
+    args = parser.parse_args()
+
+    extract_bullets(str(args.input))
+
+
